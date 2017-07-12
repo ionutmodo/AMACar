824959a7fae06577bc22e1b1c4460473121f6482
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2017-07-13 00:15:24 +0300

    Added Defines module

---
+++ AMADefines/AMADefines.h
@@ -0,0 +1,16 @@
+#ifndef __AMADefines__
+#define __AMADefines__
+
+/*
+ * Used in AMATools to allow working with Arduino Uno
+ */
+#define __ARDUINO_UNO__ true
+#define __ARDUINO_UNO_PINS_COUNT__ 14
+
+#define __DEBUG__ true
+
+#define HALF_SECOND 500
+#define ONE_SECOND 1000
+
+
+#endif
