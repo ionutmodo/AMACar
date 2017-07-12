4d14e9b4321f001438fa8cb3ec0a2aa1a026ef3a
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2017-07-13 00:16:44 +0300

    Added PWM module

---
+++ AMAPWM/AMAPWM.cpp
@@ -0,0 +1,54 @@
+#include "AMAPWM.h"
+
+using namespace AMACar;
+
+/*
+ * Generates a PWM with a specified duty cycle on a specified pin
+ */
+AMAPWM::AMAPWM(byte pin, byte dutyCycle)
+{
+    this->pin = pin;
+    this->dutyCycle = dutyCycle;
+    pinMode(pin, OUTPUT);
+}
+
+/*
+ * Sets a new pin for PWM
+ */
+void AMAPWM::setPin(byte pin)
+{
+    if(AMATools::isValidPWMPin(pin))
+    {
+        if(this->pin != pin)
+        {
+            digitalWrite(this->pin, LOW);
+            this->pin = pin;
+            pinMode(pin, OUTPUT);
+            generate();
+        }
+    }
+}
+
+/*
+ * Sets a new duty cycle
+ */
+void AMAPWM::setDutyCycle(byte dutyCycle)
+{
+    if(this->dutyCycle != dutyCycle)
+    {
+        this->dutyCycle = dutyCycle;
+        generate();
+    }
+}
+
+/*
+ * Generates the PWM 
+ */
+void AMAPWM::generate()
+{
+    analogWrite(pin, LOW);
+    delay(5);
+    byte byteValue = (byte) (2.55f * dutyCycle);
+    analogWrite(pin, byteValue);
+}
+
